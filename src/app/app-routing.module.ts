1be48e6f25b8ff1c02414935d2932eff4ed88942
Erick Chandra <mrerickchandra95@gmail.com>
2020-02-02 12:39:18 +0800

    (feat) add main page (#3)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { CategoryComponent } from './category/category.component';
+import { WalletComponent } from './wallet/wallet.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'wallet', component: WalletComponent },
+  { path: 'category', component: CategoryComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const routingComponents = [DashboardComponent, WalletComponent, CategoryComponent]
