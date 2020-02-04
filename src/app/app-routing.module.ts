433ff37a61640fa56063162baf10da9a54b6c989
Erick <mrerickchandra95@gmail.com>
2020-02-04 20:54:17 +0800

    fix import

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,8 +7,8 @@ import { DashboardsModule } from './modules/dashboards/dashboards.module';
 import { WalletComponent } from './modules/wallets/wallet/wallet.component';
 import { WalletsModule } from './modules/wallets/wallets.module';
 
-import { CategoryComponent } from './modules/categories/category/category.component';
 import { CategoriesModule } from './modules/categories/categories.module';
+import { CategoryComponent } from './modules/categories/category/category.component';
 
 const routes: Routes = [
   { path: 'dashboard', component: DashboardComponent },
