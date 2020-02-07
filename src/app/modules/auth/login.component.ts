724df0f1b19166655984b2372fada5ffd6bebb9d
Erick Chandra <mrerickchandra95@gmail.com>
2020-02-07 23:04:24 +0800

    (feat) Add login module (#15)
    * (feat) add login module and page
    
    * add function
    
    * fix
    
    * fix test and lint

---
+++ src/app/modules/auth/login.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { auth } from 'firebase';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent {
+
+  constructor(public afAuth: AngularFireAuth) { }
+
+  login() {
+    this.afAuth.auth.signInWithPopup(new auth.GoogleAuthProvider());
+  }
+
+}
