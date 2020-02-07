d31c1b3fa06e81d98fa34ce8ecdc0f4eacaeb0ca
Erick <mrerickchandra95@gmail.com>
2020-02-07 22:19:31 +0800

    fix

--- src/app/modules/auth/auth.module.ts
+++ src/app/modules/auth/auth.module.ts
@@ -6,6 +6,9 @@ import {
 } from '@angular/material';
 import { AuthComponent } from './auth.component';
 import { LoginComponent } from './login.component';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { environment } from 'src/environments/environment';
 
 @NgModule({
   declarations: [AuthComponent, LoginComponent],
@@ -13,6 +16,8 @@ import { LoginComponent } from './login.component';
     CommonModule,
     MatButtonModule,
     MatCardModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireAuthModule,
   ],
   exports: [AuthComponent, LoginComponent],
 })
