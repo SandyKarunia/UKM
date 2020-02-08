f9e5cd78fb537937d2f8edf2ae6e8809318ad551
Sandy Karunia <sandykarunia94@gmail.com>
2020-02-08 16:58:19 +0800

    Add read from firebase-config.json file

---
+++ scripts/setup-fake-firebase-config.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -eo pipefail
+
+text = `{
+    apiKey: 'FAKE_KEY',
+    authDomain: 'FAKE_AUTH_DOMAIN',
+    databaseURL: 'FAKE_DB_URL',
+    projectId: 'FAKE_PROJECT_ID',
+    storageBucket: 'FAKE_STORAGE_BUCKET',
+    messagingSenderId: 'FAKE_MESSAGING_SENDER_ID',
+    appId: 'FAKE_APP_ID',
+    measurementId: 'FAKE_MEASUREMENT_ID'
+}`;
+
+echo ${text} > ./src/environments/firebase-config.json
