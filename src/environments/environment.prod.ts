b09e311f252375f2316fa1171731e9732a148735
Sandy Karunia <sandykarunia94@gmail.com>
2020-02-08 17:19:52 +0800

    test put api key

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,7 +4,7 @@ export const environment: { production: boolean; firebase: FirebaseOptions } = {
   production: true,
   get firebase(): FirebaseOptions {
     return {
-      apiKey: '{{UKM_FIREBASE_API_KEY}}',
+      apiKey: '{{fb1d772e-b71f-45e1-9dae-6cb1b5ef4126}}',
       authDomain: '{{UKM_FIREBASE_AUTH_DOMAIN}}',
       databaseURL: '{{UKM_FIREBASE_DATABASE_URL}}',
       projectId: '{{UKM_FIREBASE_PROJECT_ID}}',
