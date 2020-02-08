cd710d54ea9ea655af00c5aede29b68c83b27f1a
Sandy Karunia <sandykarunia94@gmail.com>
2020-02-08 17:25:40 +0800

    try

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -5,13 +5,13 @@ export const environment: { production: boolean; firebase: FirebaseOptions } = {
   get firebase(): FirebaseOptions {
     return {
       apiKey: 'AIzaSyCuA9X0o22Wq_C8fz9KZqSJ9EyuM7ggr8c',
-      authDomain: '{{UKM_FIREBASE_AUTH_DOMAIN}}',
-      databaseURL: '{{UKM_FIREBASE_DATABASE_URL}}',
-      projectId: '{{UKM_FIREBASE_PROJECT_ID}}',
-      storageBucket: '{{UKM_FIREBASE_STORAGE_BUCKET}}',
-      messagingSenderId: '{{UKM_FIREBASE_MESSAGING_SENDER_ID}}',
-      appId: '{{UKM_FIREBASE_APP_ID}}',
-      measurementId: '{{UKM_FIREBASE_MEASUREMENT_ID}}',
+      authDomain: 'uangkukemana.firebaseapp.com',
+      databaseURL: 'https: //uangkukemana.firebaseio.com',
+      projectId: 'uangkukemana',
+      storageBucket: 'uangkukemana.appspot.com',
+      messagingSenderId: '897714515879',
+      appId: '1: 897714515879:web: 8bfdb9c2f68a9c47c51082',
+      measurementId: 'G-K2BC6HBZWR'
     };
   }
 };
