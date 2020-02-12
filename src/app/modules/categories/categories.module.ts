ea99092d28e713bf511d8aa4c1d7061942849e54
Sandy Karunia <sandykarunia94@gmail.com>
2020-02-13 01:00:25 +0800

    remove unnecessary changes

--- src/app/modules/categories/categories.module.ts
+++ src/app/modules/categories/categories.module.ts
@@ -1,14 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
-import { AngularFirestoreModule } from '@angular/fire/firestore';
 import { CategoryComponent } from './category/category.component';
 
 @NgModule({
   declarations: [CategoryComponent],
   imports: [
     CommonModule,
-    AngularFirestoreModule,
   ],
   exports: [CategoryComponent]
 })
