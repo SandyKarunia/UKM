53f013535dfd9b23a5ac0277f7ad372cca6ea4e9
Erick <mrerickchandra95@gmail.com>
2020-02-19 22:32:15 +0800

    fix codefactor

--- src/app/page/wallet-show-page/wallet-show-page.component.spec.ts
+++ src/app/page/wallet-show-page/wallet-show-page.component.spec.ts
@@ -2,11 +2,11 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { WalletShowPageComponent } from './wallet-show-page.component';
 
-describe('WalletShowPageComponent', () => {
+describe('WalletShowPageComponent', (): void => {
   let component: WalletShowPageComponent;
   let fixture: ComponentFixture<WalletShowPageComponent>;
 
-  beforeEach(async () => {
+  beforeEach(async (): Promise<void> => {
     await TestBed.configureTestingModule({
       declarations: [WalletShowPageComponent]
     })
