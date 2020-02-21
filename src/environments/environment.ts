4f8c1c6f063ff5fa910aa1e1d4d89ce17248b01e
Sandy Karunia <SandyKarunia@users.noreply.github.com>
2020-02-21 10:44:39 +0800

    feat: Use firebase dev project for development (#37)
    * Use firebase dev project for development
    
    * modify firebaserc

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,4 @@
 import { FirebaseOptions } from '@angular/fire';
-// tslint:disable-next-line
-import firebaseConfig from './firebase-config.json';
 
 // This file can be replaced during build by using the `fileReplacements` array.
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
@@ -9,7 +7,16 @@ import firebaseConfig from './firebase-config.json';
 export const environment: { production: boolean; firebase: FirebaseOptions } = {
   production: false,
   get firebase(): FirebaseOptions {
-    return firebaseConfig;
+    return {
+      apiKey: 'AIzaSyD_OSCYFzWgd3G8L6_ZAIiJynKCQwx5XQ8',
+      authDomain: 'uangkukemana-dev.firebaseapp.com',
+      databaseURL: 'https://uangkukemana-dev.firebaseio.com',
+      projectId: 'uangkukemana-dev',
+      storageBucket: 'uangkukemana-dev.appspot.com',
+      messagingSenderId: '750716589942',
+      appId: '1:750716589942:web:3820c546f48ce92ba1cc57',
+      measurementId: 'G-CV5DQ6P1F8'
+    };
   }
 };
 
